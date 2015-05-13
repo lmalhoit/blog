1180207cace03042bcf6fd4c7f8d6367c525ece4
unknown <lmalhoit@LMALHOIT-WS.cisco.com>
2015-05-13 12:25:15 -0400

    added to manage.py

--- manage.py
+++ manage.py
@@ -18,7 +18,12 @@ if __name__ == "__main__":
 
 @manager.command
 def seed():
-    content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    content = """
+    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    	"""
 
     for i in range(25):
         post = Post(
@@ -27,4 +32,6 @@ def seed():
         )
         session.add(post)
     session.commit()
-    
+
+if __name__ == "__main__":
+	manager.run()
